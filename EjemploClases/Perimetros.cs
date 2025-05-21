6206d10cf103c9c0d44fe365568a3e285b79a662
PILLINBOT <sistemasluis7@gmail.com>
2025-05-21 16:48:21 -0500

    Listo

---
+++ EjemploClases/Perimetros.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploClases
+{
+   public class Perimetros
+    {
+        public void PerimetroCirculo(double r, ref double p)
+        //{
+        //    double a = 2 * (Math.PI * r);
+        //    return a;
+        //}
+        {
+            p = 2 * Math.PI * r;
+        }
+
+        public void PerimetroRectangulo(double b, double h, ref double p)
+        {
+            p = 2 * (b + h);
+        }
+    }
+}
