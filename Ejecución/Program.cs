d46ca11f7d22762e4a4f6431d3aab33b279e5765
PILLINBOT <sistemasluis7@gmail.com>
2025-05-21 17:41:09 -0500

    Todo

---
+++ Ejecución/Program.cs
@@ -0,0 +1,17 @@
+﻿using EjemploClases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejecución
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
